801af2746e370d60a5183e32c792e60071b145d5
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-07-09 13:23:47 +0300

    isNumeric

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -4,11 +4,19 @@
  */
 
 function isNumeric(str) {
-  // your code
+    let result;
+    for (let i = 0; i < str.length; i++) {
+        if (Number(str)) {
+            result = true
+        } else {
+            result = false
+        }
+    }
+    return result
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
+console.log(isNumeric("abc")) // Ожидаемый результат: false
+console.log(isNumeric(" ")) // Ожидаемый результат: false

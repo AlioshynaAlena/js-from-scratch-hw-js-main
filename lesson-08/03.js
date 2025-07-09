d59b31ef03939573865553a79d0836d0b3b299e8
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-07-09 13:33:16 +0300

    truncate

--- lesson-08/03.js
+++ lesson-08/03.js
@@ -10,5 +10,16 @@
 */
 
 function truncate(str, maxLength) {
-  // your code
+    let result;
+    for (let i = 0; i < str.length; i++) {
+        if (str.length > maxLength) {
+            result = str.slice(0, 20, ) + "..."
+        } else {
+            result = str
+        }
+    }
+    return result
 }
+
+console.log(truncate("Вот, что мне действительно нравится в этом", 20)); //// Ожидаемый результат: "Вот, что мне действи..."
+console.log(truncate("Короткая строка", 20)); // Ожидаемый результат: "Короткая строка"
